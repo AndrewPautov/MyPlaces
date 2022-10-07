e84f854526ea5603970b3295cffa5a8e22e1577a
AndrewPautov <apautov04@gmail.com>
2022-10-08 00:12:51 +0300

    Creating TableViewController

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -7,7 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
